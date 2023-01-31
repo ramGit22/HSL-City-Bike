6bf6c00a3a9af807a3202a1d62b99db8e2247134
Ramesh <karki_ram@hotmail.com>
2023-01-31 20:16:59 +0200

    search functionality

--- client/hsl-city-app/src/components/JourneyTable.js
+++ client/hsl-city-app/src/components/JourneyTable.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import Pagination from 'react-bootstrap/Pagination';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavigationBar from './NavigationBar';
 
 import axios from 'axios';
 
@@ -9,11 +10,12 @@ const JourneyTable = () => {
   const [journeyData, setJourneyData] = useState({});
   const [activePage, setActivePage] = useState(1);
   const [itemsPerPage] = useState(20);
+  const [searchTerm, setSearchTerm] = useState('');
   const { departure, returnStation, distance, duration } = journeyData;
 
   useEffect(() => {
     async function fetchData() {
-      const response = await axios.get(`http://localhost:3000`);
+      const response = await axios.get(`http://localhost:3000/journey`);
       setJourneyData(response.data);
     }
     fetchData();
@@ -23,22 +25,63 @@ const JourneyTable = () => {
     return <div>Loading...</div>;
   }
 
+  const filteredDeparture = departure.filter((d) =>
+    d.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const filteredReturn = returnStation.filter((r) =>
+    r.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const filteredDistance = distance.filter((d) =>
+    d.toString().includes(searchTerm)
+  );
+  const filteredDuration = duration.filter((d) =>
+    d.toString().includes(searchTerm)
+  );
+
   const indexOfLastItem = activePage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentDeparture = departure.slice(indexOfFirstItem, indexOfLastItem);
-  const currentReturn = returnStation.slice(indexOfFirstItem, indexOfLastItem);
-  const currentDistance = distance.slice(indexOfFirstItem, indexOfLastItem);
-  const currentDuration = duration.slice(indexOfFirstItem, indexOfLastItem);
+  const currentDeparture = filteredDeparture.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+  const currentReturn = filteredReturn.slice(indexOfFirstItem, indexOfLastItem);
+  const currentDistance = filteredDistance.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+  const currentDuration = filteredDuration.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
 
   const handlePageChange = (pageNumber) => setActivePage(pageNumber);
 
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(departure.length / itemsPerPage); i++) {
+  for (
+    let i = 1;
+    i <= Math.ceil(filteredDeparture.length / itemsPerPage);
+    i++
+  ) {
     pageNumbers.push(i);
   }
 
   return (
     <>
+      <NavigationBar />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search..."
+          style={{
+            width: '25%',
+            marginTop: '20px',
+            marginBottom: '5px',
+            padding: '10px',
+          }}
+        />
+      </div>
       <Table striped bordered hover>
         <thead>
           <tr>
